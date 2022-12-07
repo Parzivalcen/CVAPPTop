974860e7638527ab82994535a2dc9834cc6675c4
David Centurion <dencen9@gmail.com>
2022-12-07 05:23:15 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Component, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import GeneralInfo from './Components/GeneralInfo'
+import './App.css'
+
+class App extends Component{
+  render(){
+
+    return(
+      <main>
+        <h1>Create your awesome CV</h1>
+        <div className='cv-components'>
+          
+        </div>
+      </main>
+    )
+  }
+}
+
+export default App
